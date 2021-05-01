2c38e31d0dbef189012df494d612dbae15148ba3
AbhishekB16 <2018csb1065@iitrpr.ac.in>
2021-05-01 22:26:19 +0530

    Revert "random"
    This reverts commit 73529c2e0d3fa2095bd3ed64071fd54174715364.

--- home/models.py
+++ home/models.py
@@ -14,9 +14,7 @@ class message(models.Model):
     to=models.CharField(max_length=145)
     msg=models.CharField(max_length=1405)
     delivered=models.IntegerField(default=0)
-    rply=message(default="null")
     date_created=models.DateTimeField()
     def __str__(self):
         return self.msg
 # Create your models here.
-# JWT based authentication
